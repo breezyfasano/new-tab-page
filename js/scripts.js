57169e822755740e71b4c9a1b9369421479c653c
Breezy Fasano <breezyfasano@gmail.com>
2019-01-03 23:39:22 -0600

    only commit items inside docs folder

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+'use strict';
+
+function getCurrentTime() {
+    let time = new Date().getHours();
+    if (time >= 1 && time <= 3) return "evening";
+    if (time >= 4 && time <= 11) return 'morning';
+    if (time >= 12 && time <= 16) return 'afternoon';
+    if (time >= 17 && time <= 24) return 'evening';
+    console.log(time);
+}
+
+const greeting = document.getElementById('greeting');
+greeting.textContent = `Good ${getCurrentTime()}, Breezy!`;
+
+fetch("http://quotes.rest/qod.json?category=inspire") // fetching the quote of the day API
+.then(response => {
+    return response.json();
+})
+.then(json => {
+    // accesses the quote of the day, and injects the quote and author into the HTML
+    let qod = json.contents.quotes[0];
+    const quoteBox = document.querySelector('#qod p');
+    const quoteCite = document.querySelector("cite");
+    quoteBox.textContent = qod.quote;
+    quoteCite.textContent = qod.author;
+});
